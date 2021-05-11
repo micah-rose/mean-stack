25170ff63935b768bfa98c9a10129dfa7a7aa0ea
Micah Lund <micah.r.lund@gmail.com>
2021-05-11 11:57:53 -0600

    Created a component to list the posts on our page.

--- mean-frontend/src/app/app.module.ts
+++ mean-frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
@@ -15,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
