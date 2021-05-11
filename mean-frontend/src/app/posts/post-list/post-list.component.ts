bbb4dc21fe02300541a83e787e3c1671f8b1a6cc
Micah Lund <micah.r.lund@gmail.com>
2021-05-11 14:36:47 -0600

    Set up the logic for getting posts.

--- mean-frontend/src/app/posts/post-list/post-list.component.ts
+++ mean-frontend/src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit} from "@angular/core";
+import { Component, OnInit, OnDestroy} from "@angular/core";
+import { Subscription } from 'rxjs';
 import { Post } from '../post.model';
 import { PostService } from '../posts.service';
 
@@ -7,7 +8,7 @@ import { PostService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit{
+export class PostListComponent implements OnInit, OnDestroy{
   // posts = [
   //   {title: 'First Post', content: 'This is the first post content.'},
   //   {title: 'Second Post', content: 'This is the second post content.'},
@@ -15,10 +16,19 @@ export class PostListComponent implements OnInit{
   // ];
 
   posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postService: PostService){}
 
   ngOnInit(){
     this.posts = this.postService.getPosts();
+    this.postsSub = this.postService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+    });
+  };
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
   };
 }
