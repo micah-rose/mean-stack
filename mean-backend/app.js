e525f3ab3956117ecb126dbe620f6fb7edabd72b
Micah Lund <micah.r.lund@gmail.com>
2021-05-12 10:07:35 -0600

    Created a post instance inour app file.

--- mean-backend/app.js
+++ mean-backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const Post = require('./models/post');
+
 const app = express();
 app.use(express.json());
 
@@ -17,7 +19,10 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-    const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content 
+    });
     console.log(post);
     res.status(201).json({
         message: 'Post added successfully'
