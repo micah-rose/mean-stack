f5c4ca020d8040938be02ee896ed30a88eef510c
Micah Lund <micah.r.lund@gmail.com>
2021-05-12 11:00:07 -0600

    Transformed the response data to remove the underscore from the id.

--- mean-frontend/src/app/posts/posts.service.ts
+++ mean-frontend/src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -12,9 +13,19 @@ export class PostService {
 
   getPosts(){
     this.http
-      .get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .get<{message: string, posts: any}>(
+        'http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -25,7 +36,8 @@ export class PostService {
 
   addPost(title: string, content: string){
     const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{message: string}>(
+      'http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
         console.log(responseData.message);
         this.posts.push(post);
