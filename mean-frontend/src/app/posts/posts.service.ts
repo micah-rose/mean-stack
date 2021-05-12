e938a473c444b672fc8080bfb2905f142092882b
Micah Lund <micah.r.lund@gmail.com>
2021-05-12 16:53:49 -0600

    Added logic to update posts on the server.

--- mean-frontend/src/app/posts/posts.service.ts
+++ mean-frontend/src/app/posts/posts.service.ts
@@ -36,7 +36,7 @@ export class PostService {
   }
 
   getSinglePost(id: string){
-    return {...this.posts.find(post => post.id === id)}
+    return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id);
   }
 
   addPost(title: string, content: string){
@@ -55,7 +55,13 @@ export class PostService {
   updatePost(id: string, title: string, content: string){
     const post: Post = {id: id, title: title, content: content};
     this.http.put('http://localhost:3000/api/posts/' + id, post)
-      .subscribe(response => console.log(response));
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   deletePost(postId: string){
