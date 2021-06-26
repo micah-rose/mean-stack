1ad9d476046746314e55ac89aedbff5dcd0ce5e8
Micah Lund <micah.r.lund@gmail.com>
2021-06-25 19:28:49 -0600

    Push project to repo from new machine.

--- mean-backend/app.js
+++ mean-backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('../config/db');
-
 const postsRoutes = require('./routes/posts');
 
 const app = express();
@@ -10,11 +9,11 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-        'Access-Control-Allow-Headers', 
+        'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
     );
     res.setHeader(
-        'Access-Control-Allow-Methods', 
+        'Access-Control-Allow-Methods',
         'GET, POST, PUT, PATCH, DELETE, OPTIONS'
     );
     next();
